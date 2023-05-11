790f5e4fa7114af629fd23a37288d633bdc342ee
keeganpearson <keegan.dpearson@gmail.com>
2023-05-11 20:37:43 +0000

    Function that acts as a command line alarm clock and makes a system beep after time runs out

---
+++ timer1.js
@@ -0,0 +1,20 @@
+// slice off the node thingy to get command line arguments
+let args = process.argv.slice(2);
+
+// for each argument do this
+args.forEach(arg => {
+  // convert to number
+  let time = Number(arg);
+
+  // check if greater than 0, ignore if NaN or negative
+  if (!isNaN(time) && time >= 0) {
+    // convert given time to milliseconds to make easier
+    time *= 1000;
+
+    // set timer
+    setTimeout(() => {
+      // when time expires, beep
+      process.stdout.write('\x07');
+    }, time);
+  }
+});
